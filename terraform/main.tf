4b23779b70bc882dd6531a6482ecf76efaabae98
Rodrigo Davila <rodrigordavila@gmail.com>
2022-10-22 17:18:18 -0300

    terraform update

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,21 +1,39 @@
 terraform {
   required_providers {
     digitalocean = {
-      source = "digitalocean/digitalocean"
+      source  = "digitalocean/digitalocean"
       version = "~> 2.0"
     }
   }
 }
 
 provider "digitalocean" {
-  token = "dop_v1_5639a0908e22c1cfe4d9660a202d2325bd35037edcdae5ed6f268c5b784fce6b"
+  token = ""
 }
 
-# Create a new Web Droplet in the nyc2 region
+# Create a new Web Droplet in the nyc1 region
 resource "digitalocean_droplet" "jenkins" {
-  image  = "ubuntu-22-04-x64"
-  name   = "jenkins"
-  region = "nyc1"
-  size   = "s-2vcpu-2gb"
+  image    = "ubuntu-22-04-x64"
+  name     = "jenkins"
+  region   = "nyc1"
+  size     = "s-2vcpu-2gb"
+  ssh_keys = [data.digitalocean_ssh_key.rodrigo.id]
 }
 
+data "digitalocean_ssh_key" "rodrigo" {
+  name = "rodrigo"
+}
+
+resource "digitalocean_kubernetes_cluster" "k8s" {
+  name   = "k8s"
+  region = "nyc1"
+  # Grab the latest version slug from `doctl kubernetes options versions`
+  version = "1.24.4-do.0"
+
+  node_pool {
+    name       = "worker-pool"
+    size       = "s-2vcpu-2gb"
+    node_count = 2
+
+  }
+}
